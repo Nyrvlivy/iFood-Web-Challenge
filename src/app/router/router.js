e0322b13f511e0793a1342b067dc5cd35fa354c1
Nyrvlivy <nyrvlivy@gmail.com>
2024-09-15 09:52:04 -0300

    chore(url): update URLs to portuguese

--- src/app/router/router.js
+++ src/app/router/router.js
@@ -1,15 +1,35 @@
+const routeMap = {
+    "dashboard": "dashboard",
+    "settlements": "repasses",
+    "entries": "lancamentos",
+    "performance": "desempenho",
+    "operation-health": "saude-operacional",
+    "goals-and-projections": "metas-e-projecoes",
+};
+
+function getRouteInEnglish(route) {
+    return Object.keys(routeMap).find((key) => routeMap[key] === route) || route;
+}
+
+function getRouteInPortuguese(route) {
+    return routeMap[route] || route;
+}
+
 function navigateTo(route) {
     const appContent = document.getElementById("app-content");
-    let pageUrl = `src/app/pages/${route}/${route}.html`;
-    let cssUrl = `src/app/pages/${route}/${route}.css`;
+
+    let routeInEnglish = getRouteInEnglish(route);
+    let pageUrl = `src/app/pages/${routeInEnglish}/${routeInEnglish}.html`;
+    let cssUrl = `src/app/pages/${routeInEnglish}/${routeInEnglish}.css`;
 
     fetch(pageUrl)
         .then((response) => response.text())
         .then((html) => {
             appContent.innerHTML = html;
 
-            if (window.location.hash !== `#${route}`) {
-                window.location.hash = `#${route}`;
+            const newHash = getRouteInPortuguese(routeInEnglish);
+            if (window.location.hash !== `#/${newHash}`) {
+                window.history.replaceState(null, null, `#/${newHash}`);
             }
 
             let existingLink = document.getElementById("page-specific-css");
@@ -31,11 +51,11 @@ function navigateTo(route) {
 }
 
 window.addEventListener("load", () => {
-    const path = window.location.hash.substring(1) || "dashboard";
+    const path = getRouteInEnglish(window.location.hash.substring(2)) || "dashboard";
     navigateTo(path);
 });
 
 window.addEventListener("hashchange", () => {
-    const path = window.location.hash.substring(1);
+    const path = getRouteInEnglish(window.location.hash.substring(2));
     navigateTo(path);
 });
